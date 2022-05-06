a5d4cd66bb1ee155fa8d87ddc651ff1dcf4c4606
braingremlin <31418382+braingremlin85@users.noreply.github.com>
2022-05-06 09:55:33 +0200

    initial commit

---
+++ hostsgen.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# EVILHOSTS
+# Generate a hosts file from internet lists of malicious url
+
+# CONFIG START
+WORKING_DIR="/dir/to/script/evilhosts/"
+EXPORTFILE="/yourwww/hosts.txt"
+EXCLUDESFILE="exclude.list"
+
+HOSTSLISTS=( 
+	"http://someonewhocares.org/hosts/hosts"
+	#"http://www.malwaredomainlist.com/hostslist/hosts.txt" blank list atm
+	"https://urlhaus.abuse.ch/downloads/hostfile/"
+) 
+
+# CONFIG END
+
+ERRCOUNT=0
+
+cd $WORKING_DIR
+
+rm $EXPORTFILE
+
+touch $EXPORTFILE
+
+mkdir -p 'lists'
+
+mapfile -t EXCLUDELIST < "$EXCLUDESFILE" 
+
+#read -r -a EXCLUDELIST <<< sed -e "/^\s*#/d;/^[[:space:]]*$/d" "$EXCLUDESFILE"
+#echo "${EXCLUDELIST[*]}"
+#exit
+
+for i in "${HOSTSLISTS[@]}" 
+do 
+	FILENAME=$(echo "$i" | awk -F/ '{print $3}') #extract domain name to name file
+
+	printf "Downloading $FILENAME...\n"
+	#wget $i -O "lists/"$FILENAME -q
+	
+	if [ $? -eq 0 ]; then
+		printf "$FILENAME downloaded successfully\n"
+		logger "EVILHOSTS: $FILENAME downloaded successfully\n"
+	else
+		printf "Download of $FILENAME failed\n"
+		logger "EVILHOSTS: Download of $FILENAME failed"
+		((ERRCOUNT++))
+	fi
+	
+	IFS='|' ;sed -E "/${EXCLUDELIST[*]}/d;/^\s*#/d;/^[[:space:]]*$/d" "lists/$FILENAME" >> $EXPORTFILE
+
+done
+sort -u -o $EXPORTFILE $EXPORTFILE
+
+#headers for hosts file
+sed -i "1 i\#$(date)" $EXPORTFILE
+sed -i "2 i\#evilhosts hosts file generator by braingremlin" $EXPORTFILE
+
+if [ $? -eq 0 ]; then
+	printf "$EXPORTFILE exported successfully\n"
+	logger "EVILHOSTS: $EXPORTFILE exported successfully\n"
+else
+	printf "Export of $FILENAME failed\n"
+	logger "EVILHOSTS: Export of $FILENAME failed"
+	((ERRCOUNT++))
+fi
+
+if [ $ERRCOUNT -eq 0 ]; then
+	#succeded
+	printf "$EXPORTFILE exported without errors\n"
+	logger "EVILHOSTS: $EXPORTFILE exported without errors"
+else
+	#errors
+	printf "EVILHOSTS parsing throw $ERRCOUNT error\n"
+	logger "EVILHOSTS parsing throw $ERRCOUNT error"
+fi
